c64718158e79454826f5d97a7ddf664c46096894
johanz97 <ejzambrano4@espe.edu.ec>
2021-10-19 11:11:28 -0500

    estado inicial

---
+++ android/app/src/main/kotlin/com/example/estadosapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.estadosapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/pagina1_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Pagina1Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pagina 1'),
+      ),
+      body: InformacionUsuario(),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.pages),
+          onPressed: () => Navigator.pushNamed(context, 'pagina2')),
+    );
+  }
+}
+
+class InformacionUsuario extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            'General',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Nombre: '),
+          ),
+          ListTile(
+            title: Text('Edad: '),
+          ),
+          Text(
+            'Profesiones',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Profesion 1: '),
+          ),
+          ListTile(
+            title: Text('Profesion 2: '),
+          ),
+        ],
+      ),
+    );
+  }
+}
